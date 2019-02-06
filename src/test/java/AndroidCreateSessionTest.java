9af92583eb9cec64266c65bce2e288a51475f4f7
CosminFirsov <cosmin43@gmail.com>
2019-02-06 16:46:59 +0100

    Add files via upload
    Primer commit

---
+++ src/test/java/AndroidCreateSessionTest.java
@@ -0,0 +1,99 @@
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.appium.java_client.android.Activity;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+
+public class AndroidCreateSessionTest {
+    private static AndroidDriver<WebElement> driver;
+    private static AppiumDriverLocalService service;
+
+    private final String SEARCH_ACTIVITY = ".app.SearchInvoke";
+
+    private final String ALERT_DIALOG_ACTIVITY = ".app.AlertDialogSamples";
+
+    private final String PACKAGE = "io.appium.android.apis";
+
+    @BeforeClass
+    public static void setUp() throws Exception {
+//        service = AppiumDriverLocalService.buildDefaultService();
+//        System.out.println("hola2222222");
+        
+//        service.start();
+        
+        File app = new File("D:\\Android\\ApiDemos-debug.apk");
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        capabilities.setCapability("deviceName", "Test");
+        capabilities.setCapability("app", app.getAbsolutePath());
+        capabilities.setCapability("appPackage", "io.appium.android.apis");
+        capabilities.setCapability("appActivity", ".ApiDemos");
+
+        driver = new AndroidDriver<WebElement>(getServiceUrl(), capabilities);
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        driver.quit();
+//        service.stop();
+    }
+
+    @Test()
+    public void testCreateSession() {
+        String activity = driver.currentActivity();
+        String pkg = driver.getCurrentPackage();
+
+//        Assert.assertEquals(activity, ".ApiDemos");
+        Assert.assertEquals(pkg, "io.appium.android.apis");
+    }
+    
+    @Test()
+
+    public void testSendKeys() {
+        driver.startActivity(new Activity(PACKAGE, SEARCH_ACTIVITY));
+        AndroidElement searchBoxEl = (AndroidElement) driver.findElementById("txt_query_prefill");
+        searchBoxEl.sendKeys("Hello world!");
+        AndroidElement onSearchRequestedBtn = (AndroidElement) driver.findElementById("btn_start_search");
+        onSearchRequestedBtn.click();
+        AndroidElement searchText = (AndroidElement) new WebDriverWait(driver, 30)
+                .until(ExpectedConditions.visibilityOfElementLocated(By.id("android:id/search_src_text")));
+        String searchTextValue = searchText.getText();
+
+        Assert.assertEquals(searchTextValue, "Hello world!");
+    }
+
+    @Test
+    public void testOpensAlert() {
+        // Open the "Alert Dialog" activity of the android app
+        driver.startActivity(new Activity(PACKAGE, ALERT_DIALOG_ACTIVITY));
+        // Click button that opens a dialog
+        AndroidElement openDialogButton = (AndroidElement) driver.findElementById("io.appium.android.apis:id/two_buttons");
+        openDialogButton.click();
+        // Check that the dialog is there
+        AndroidElement alertElement = (AndroidElement) driver.findElementById("android:id/alertTitle");
+        String alertText = alertElement.getText();
+        Assert.assertEquals(alertText, "Lorem ipsum dolor sit aie consectetur adipiscing\nPlloaso mako nuto siwuf cakso dodtos anr koop.");
+        AndroidElement closeDialogButton = (AndroidElement) driver.findElementById("android:id/button1");
+        // Close the dialog
+        closeDialogButton.click();
+    }
+    
+    public static URL getServiceUrl () throws MalformedURLException {
+//        return service.getUrl();
+    	return new URL("http://127.0.0.1:4723/wd/hub");
+    }
+	
+}
